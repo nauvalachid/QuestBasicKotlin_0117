c9be68a48d6a3aa4a35fb9773f77b8672758916f
nauvalachid <nauvalachid@gmail.com>
2024-10-13 05:56:47 +0700

    membuat function withDefaultParameter,withReturn,main

--- app/src/main/java/com/example/test_pam/Functions.kt
+++ app/src/main/java/com/example/test_pam/Functions.kt
@@ -19,3 +19,21 @@ fun withNamedArgument(name: String, age: Int){
     println("Hello, $name! You are $age years old.")
 }
 
+//default parameter value
+//default parameter value merupakan cara untuk memberikan nilai default pada parameter fungsi
+fun withDefaultParameter(name: String = "Indra", age: Int) {
+    println()
+    println("== withDefaultParameter ==")
+    println("Hello, $name! You are $age years old.")
+}
+
+fun withReturn(panjang : Int, lebar : Int) {
+    return println(panjang * lebar)
+}
+
+fun main(){
+    withoutParameter()
+    withParameter("John")
+    withNamedArgument(name = "Jhon", age = 30)
+    withDefaultParameter(age = 25)
+}
