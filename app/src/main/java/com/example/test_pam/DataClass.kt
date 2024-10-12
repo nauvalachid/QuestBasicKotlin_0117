fa8810573039910395f99f168e0785be0f4cdbed
nauvalachid <nauvalachid@gmail.com>
2024-10-13 05:50:43 +0700

    membuat file data class

---
+++ app/src/main/java/com/example/test_pam/DataClass.kt
@@ -0,0 +1,12 @@
+package com.example.test_pam
+
+// data class
+// data class adalah class yang digunakan untuk menyimpan data
+// data class menyediakan fungsi untuk meng-override fungsi aquals(), hashCode(), dan toString()
+// data class juga menyediakan fungsi copy() untuk membuat objek dengan beberapa perubahan
+
+data class DataClass(
+    val id : Int,
+    val email: String
+)
+
