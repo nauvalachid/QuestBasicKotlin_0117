4f0642109912e8742820163819beab110682553f
nauvalachid <nauvalachid@gmail.com>
2024-10-13 05:40:43 +0700

    menambahkan fungsi contohset & contohmap

--- app/src/main/java/com/example/test_pam/Collections.kt
+++ app/src/main/java/com/example/test_pam/Collections.kt
@@ -34,3 +34,72 @@ fun ContohList() {
     println(shapesLocked)
 }
 
+// Set adalah kumpulan data yang tidak memiliki posisi atau urutan tertentu dan hanya menyimpan data unik (tidak ada duplikasi)
+// Set bersifat unordered, artinya data yang dimasukkan tidak memiliki posisi tertentu
+// Set bersifat mutable dan read only, artinya data yang dimasukkan dapat diubah dan tidak dapat diubah
+// Set Read-Only menggunakan setOf
+// Set Mutable menggunakan mutableSetOf
+
+fun ContohSet() {
+    println("===== Set =====")
+
+    // Set Read-Only
+    val readOnlyAbjad = setOf("A", "B", "C")
+    println(readOnlyAbjad)
+
+    // Set Mutable
+    val shape: MutableSet<String> = mutableSetOf("Circle", "Square", "Triangle")
+    println(shape)
+
+    // Menambahkan data ke dalam Set Mutable
+    shape.add("Rectangle")
+    println(shape)
+
+    // Menghapus data dari Set Mutable
+    shape.remove("Circle")
+    println(shape)
+
+    // Set Read-Only
+    val shapesLocked: Set<String> = shape
+    println(shapesLocked)
+}
+
+// Map adalah kumpulan data yang disusun dalam pasangan key-value
+// Map bersifat unordered, artinya data yang dimasukkan tidak memiliki posisi tertentu
+// Map bersifat mutable dan read only, artinya data yang dimasukkan dapat diubah dan tidak dapat diubah
+// Map Read-Only menggunakan mapOf
+// Map Mutable menggunakan mutableMapOf
+
+fun ContohMap() {
+    println("===== Map =====")
+
+    // Map Read-Only
+    val readOnlyShape: Map<String, Int> = mapOf("Circle" to 1, "Square" to 2, "Triangle" to 3)
+    println(readOnlyShape)
+
+    // Map Mutable
+    val shape: MutableMap<String, Int> = mutableMapOf("Circle" to 1, "Square" to 2, "Triangle" to 3)
+    println(shape)
+
+    // Menambahkan data ke dalam Map Mutable
+    shape["Rectangle"] = 4
+    println(shape)
+
+    // Menghapus data dari Map Mutable
+    shape.remove("Circle")
+    println(shape)
+
+    // Mengubah data di dalam Map Mutable
+    shape["Square"] = 5
+    println(shape)
+
+    // Map Read-Only
+    val shapesLocked: Map<String, Int> = shape
+    println(shapesLocked)
+}
+
+fun main() {
+    ContohList()
+    ContohSet()
+    ContohMap()
+}
