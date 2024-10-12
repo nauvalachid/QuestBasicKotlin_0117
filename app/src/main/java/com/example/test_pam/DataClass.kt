070a9095f02214dc259e2572a0157d85b6744881
nauvalachid <nauvalachid@gmail.com>
2024-10-13 05:52:29 +0700

    membuat function main

--- app/src/main/java/com/example/test_pam/DataClass.kt
+++ app/src/main/java/com/example/test_pam/DataClass.kt
@@ -10,3 +10,19 @@ data class DataClass(
     val email: String
 )
 
+fun main() {
+    val data = DataClass(1, "indra@gmail.com")
+    println(data)
+
+    // fungsi equals
+    val data2 = DataClass(1, "makan@gmail.com")
+    println(data == data2) //false
+
+    // fungsi copy
+    val data3 = data.copy()
+    println(data3)
+
+    // fungsi copy dengan perubahan
+    val data4 = data.copy(email = "sahar@gmail.com")
+    println(data4)
+}
