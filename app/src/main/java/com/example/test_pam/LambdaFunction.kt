281bbfb3ec7bb436aabd27ef8c1ef68e16336fed
nauvalachid <nauvalachid@gmail.com>
2024-10-13 06:07:20 +0700

    membuat function main

--- app/src/main/java/com/example/test_pam/LambdaFunction.kt
+++ app/src/main/java/com/example/test_pam/LambdaFunction.kt
@@ -9,3 +9,9 @@ fun uppercaseString(string: String): String{
     return string.uppercase()
 }
 
+// Dapat ditulis dalam ekspresi lambda sbb :
+fun main(){
+    uppercaseString("hello")
+    println({ string: String -> string.uppercase() }("hello"))
+    //Hello
+}
